e832ec2e3ec5d44fb722f67284a7347c04ca8ab3
Josh Wolsborn <wolsborn@pdx.edu>
2020-02-06 21:45:03 -0800

    Add add_item function Add remove_item function Modify functionality of get list output

--- consoleClient/src/main.rs
+++ consoleClient/src/main.rs
@@ -3,12 +3,11 @@ use serde::{Serialize, Deserialize};
 extern crate reqwest;
 
 #[derive(Deserialize)]
-struct resBody {
+struct ResBody {
     groceries: Vec<String>,
 }
 
 fn main() {
-    get_list();
     run();
 }
 
@@ -32,7 +31,57 @@ fn run() {
 }
 
 fn get_list() -> Result<(), reqwest::Error> {
-    let body = reqwest::blocking::get("http://localhost:8000/list")?.json::<resBody>()?;
-    body.groceries.iter().map(|x| println!("{}", x));
+    let body = reqwest::blocking::get("http://localhost:8000/list")?.json::<ResBody>()?;
+
+    if body.groceries.len() != 0
+    {
+        println!("Grocery List");
+        println!("------------");
+        for item in &body.groceries {
+            println!("{}", item);
+        }
+    }
+    else
+    {
+        println!("No items in list");
+    }
+
+    Ok(())
+}
+
+fn add_item() -> Result<(), reqwest::Error> {
+    let mut item = String::new();
+
+    let stdin = io::stdin();
+    print!("Enter the item you wish to add: ");
+
+    io::stdout().flush();
+    stdin.lock().read_line(&mut item);
+
+    let mut url = "http://localhost:8000/add/".to_string();
+    url.push_str(&item.as_str());
+
+    let client = reqwest::blocking::Client::new();
+    let body = client.post(url.as_str()).send();
+
+    Ok(())
+
+}
+
+fn remove_item() -> Result<(), reqwest::Error> {
+    let mut item = String::new();
+
+    let stdin = io::stdin();
+    print!("Enter the item you wish to remove: ");
+
+    io::stdout().flush();
+    stdin.lock().read_line(&mut item);
+
+    let mut url = "http://localhost:8000/remove/".to_string();
+    url.push_str(&item.as_str());
+
+    let client = reqwest::blocking::Client::new();
+    let body = client.put(url.as_str()).send();
+
     Ok(())
 }
